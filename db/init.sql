57186bd802e4c9ea2e12bc286c5104f685a4f015
Kyla Marjes <kylamarjes11@gmail.com>
2026-03-08 21:35:02 +0800

    chore: align database init script with docker environment

--- db/init.sql
+++ db/init.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS expense_system_development CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS expense_system_test CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE USER IF NOT EXISTS 'expense_user'@'%' IDENTIFIED BY 'expense_pass';
+CREATE USER IF NOT EXISTS 'expense_user'@'%' IDENTIFIED BY 'expense_password';
 
 GRANT ALL PRIVILEGES ON expense_system_development.* TO 'expense_user'@'%';
 GRANT ALL PRIVILEGES ON expense_system_test.* TO 'expense_user'@'%';
